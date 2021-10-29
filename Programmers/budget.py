b0c7c042660fd3a6d5fa44305839a2ace228da0e
bluedog <bluedog129@gmail.com>
2021-10-29 16:24:56 +0900

    budget

---
+++ Programmers/budget.py
@@ -0,0 +1,24 @@
+d = [2,2,3,3]
+budget = 9
+
+def solution(d, budget):
+    answer = 0
+    sum = 0
+    d.sort()
+    for i in range(len(d)):
+        sum += d[i]
+
+        if budget >= sum:
+            answer += 1
+    
+    return answer
+
+print(solution(d, budget))
+
+def solution(d, budget):
+    d.sort()
+    while budget < sum(d):
+        d.pop()
+    return len(d)
+
+print(solution(d, budget))
