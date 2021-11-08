c0faee09d19b922176bffce818ab68aaf4798489
bluedog <bluedog129@gmail.com>
2021-11-08 21:38:57 +0900

    수정

--- Programmers/rectangleStar.py
+++ Programmers/rectangleStar.py
@@ -1,6 +1,6 @@
 a, b = map(int, input().strip().split(' '))
 
-for i in range(b):
-    for j in range(a):
+for i in range(len(b)):
+    for j in range(len(a)):
         print('*', end='')
     print()
