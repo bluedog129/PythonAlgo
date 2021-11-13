3cce677ea883ae985a0c035ff0885cb87a3d111c
unknown <bluedog129@gmail.com>
2021-11-13 18:35:14 +0900

    수정

--- Programmers/hashadNum.py
+++ Programmers/hashadNum.py
@@ -14,3 +14,17 @@ def solution(x):
     return answer
 
 print(solution(x))
+
+def solution(x):
+    sum = 0
+    num = x
+    while x > 0:
+        sum += x%10
+        x = int(x/10)
+        
+    if num % sum == 0:
+        return True
+    else:
+        return False
+
+print(solution(x))
