b767bff40aca6f1c1bb3eb9fc252ef8177e46e6c
unknown <bluedog129@gmail.com>
2021-11-24 19:38:55 +0900

    subarrayDivision

---
+++ hackerRank/subarrayDivision.py
@@ -0,0 +1,22 @@
+from functools import reduce
+
+s = [2,2,1,3,2]
+d = 4
+m = 2
+
+def add(a, b):
+    return a+b
+
+
+def birthday(s, d, m):
+
+    count = 0
+
+    for i in range(len(s)):
+        temp_list = s[i:i+m] # 부분 배열의 길이 m 만큼 생성
+        if reduce(add, temp_list) == d:
+            count += 1
+        
+    return count
+
+print(birthday(s, d, m))
