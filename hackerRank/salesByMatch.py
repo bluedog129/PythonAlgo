c9ac15d3af55075eba738fcd1fabf7f4d11285de
unknown <bluedog129@gmail.com>
2021-11-26 22:13:54 +0900

    salesByMatch

---
+++ hackerRank/salesByMatch.py
@@ -0,0 +1,15 @@
+n = 9
+ar = [10, 20, 20, 10, 10, 30, 50, 10, 20]
+
+def sockMerchant(n, ar):
+    ar_set = 0
+    ar_count = 0
+    
+    ar_set = set(ar)
+
+    for i in ar_set:
+        ar_count += ar.count(i)//2
+
+    return ar_count
+
+print(sockMerchant(n, ar))
