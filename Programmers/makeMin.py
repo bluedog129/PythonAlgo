830261262b50b80e20bfc165bd08e97e2b8f1a4a
CHOI HYOJONG <bluedog129@gmail.com>
2021-12-20 22:30:59 +0900

    makemin 수정

--- Programmers/makeMin.py
+++ Programmers/makeMin.py
@@ -10,7 +10,9 @@ def solution(A,B):
     for i in range(len(A)):
         answer += A[i]*B[i]
 
-
     return answer
 
 print(solution(A,B))
+
+def getMinSum(A,B):
+    return sum(a*b for a, b in zip(sorted(A), sorted(B, reverse = True)))
