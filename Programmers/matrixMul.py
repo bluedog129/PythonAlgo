a92aa5752adc69b5928e1b1308aab38537dc2676
CHOI HYOJONG <bluedog129@gmail.com>
2021-12-24 16:55:33 +0900

    수정

---
+++ Programmers/matrixMul.py
@@ -0,0 +1,15 @@
+arr1 = [[1, 4], [3, 2], [4, 1]]
+arr2 = [[3, 3], [3, 3]]
+
+def solution(arr1, arr2):
+    answer = [[]]
+
+    for i in range(len(arr1)):
+        for j in range(len(arr2)):
+            arr1[i] * arr2[j]
+            
+            
+
+    return answer
+
+print(solution(arr1, arr2))
