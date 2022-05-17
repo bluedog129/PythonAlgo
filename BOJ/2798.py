e9eac46e36fa20e7cc2e09f35ecb14fb875f16bc
chj <bluedog129@gmail.com>
2022-05-17 14:07:34 +0900

    블랙잭 수정

--- BOJ/2798.py
+++ BOJ/2798.py
@@ -1,15 +1,15 @@
 n, m = map(int, input().split())
-a = list(map(int, input().split()))
-b = len(a)
+n_list = list(map(int, input().split()))
 
+n_len = len(n_list)
 sum = 0
 
-for i in range(0, b - 2):
-        for j in range(i + 1, b - 1):
-                for k in range(j + 1, b):
-                        if a[i] + a[j] + a[k] > m:
-                                continue
-                        else:
-                                sum = max(sum, a[i] + a[j] + a[k])
+for i in range(0, n_len - 2):
+    for j in range(i + 1, n_len - 1):
+        for k in range(j + 1, n_len):
+            if (n_list[i] + n_list[j] + n_list[k]) > m:
+                continue
+            else:
+                sum = max(sum, n_list[i] + n_list[j] + n_list[k])
 
 print(sum)
