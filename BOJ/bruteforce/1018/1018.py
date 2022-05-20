68a87d2902fdae11aa00912aa3fa22e8e8e656db
chj <bluedog129@gmail.com>
2022-05-20 13:05:56 +0900

    1018 체스다시칠하기

---
+++ BOJ/bruteforce/1018/1018.py
@@ -0,0 +1,29 @@
+lows_len, cols_len = map(int, input().split())
+
+original = []
+count = []
+
+for _ in range(lows_len):
+    original.append(input())
+
+# 체스판의 시작점을 잡는다
+# a는 행, b는 열에 대하여 원래의 체스 판에서 8*8로 자를 수 있는 범위의 시작점을 가리킴
+for a in range(lows_len - 7):
+    for b in range(cols_len - 7):
+        index1 = 0
+        index2 = 0
+        for i in range(a, a + 8):
+            for j in range(b, b + 8):
+                if (i + j) % 2 == 0:
+                    if original[i][j] != 'W':
+                        index1 += 1
+                    if original[i][j] != 'B':
+                        index2 += 1
+                else:
+                    if original[i][j] != 'B':
+                        index1 += 1
+                    if original[i][j] != 'W':
+                        index2 += 1
+        count.append(min(index1, index2))
+
+print(min(count))
