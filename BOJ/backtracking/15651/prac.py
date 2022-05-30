d1c7225daf12b37d07273ae4027b3deefe6cda91
chj <bluedog129@gmail.com>
2022-05-30 15:39:29 +0900

    15651 연습

---
+++ BOJ/backtracking/15651/prac.py
@@ -0,0 +1,15 @@
+n, m = map(int, input().split())
+
+n_list = []
+
+def dfs():
+    if len(n_list) == m:
+        print(' '.join(map(str, n_list)))
+        return
+
+    for i in range(1, n+1):
+        n_list.append(i)
+        dfs()
+        n_list.pop()
+
+dfs()
