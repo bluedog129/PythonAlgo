0c9143a07ced9c1a68f646d1b99723a1fbd1032f
chj <bluedog129@gmail.com>
2022-05-30 15:09:38 +0900

    n과 m(3)

--- BOJ/backtracking/15651/15651.py
+++ BOJ/backtracking/15651/15651.py
@@ -1,12 +1,10 @@
-import sys
-sys.setrecursionlimit(10**7)
-
 n, m = map(int, input().split())
 list_n = []
 
 def dfs():
     if len(list_n) == m:
         print(' '.join(map(str, list_n)))
+        return
 
     for i in range(1, n + 1):
         list_n.append(i)
