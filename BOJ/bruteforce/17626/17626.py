33ec5f9aab7ad8fbd4c5d5bb19cda8d139e6d150
chj <bluedog129@gmail.com>
2022-06-11 23:04:30 +0900

    17626

--- BOJ/bruteforce/17626/17626.py
+++ BOJ/bruteforce/17626/17626.py
@@ -10,7 +10,7 @@ def bf(n, count):
         return
     if count > minCount:
         return
-
+    # 재귀의 깊이에 따라 n을 나눈뒤 제곱근 한 값까지만 탐
     for i in range(int(n ** 0.5), int((n // (4 - count)) ** 0.5), -1):
         bf(n - i ** 2, count + 1)
 
