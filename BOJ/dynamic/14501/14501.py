e0c518db8fbf142835ab400746a7f3368acfba7a
chj <bluedog129@gmail.com>
2022-06-15 19:12:22 +0900

    14501 퇴사

--- BOJ/dynamic/14501/14501.py
+++ BOJ/dynamic/14501/14501.py
@@ -6,13 +6,13 @@ for _ in range(n):
     a, b = map(int, input().split())
     t.append(a)
     p.append(b)
-
+# 받은 돈을 넣은 리스트
 dp = [0] * (n + 1)
 
 for i in range(n - 1, -1, -1):
-    # 리미트 날짜 < i날짜 + 걸리는 시간
+    # 정해진 날짜(7) < i날짜 + 걸리는 시간
     if n < t[i] + i:
-        # 퇴사일을 넘기는 일정은 패스
+        # 퇴사일을 넘기는 일정은 더하지 않는다
         dp[i] = dp[i + 1]
     else:
         # i날짜에 상담을 하는 것과 안 하는 것 중 큰 것을 선택
